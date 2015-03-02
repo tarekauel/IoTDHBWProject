53632040743e0580bdf91846c0e160b4dbc6876c
Tarek Auel <tarek.auel@googlemail.com>
2015-03-02 15:00:01 +0100

    initial commit

---
+++ src/main/java/de/dhbw/mannheim/iot/HelloWorld.java
@@ -0,0 +1,17 @@
+package de.dhbw.mannheim.iot;
+
+import com.google.gson.Gson;
+
+/**
+ * @author Tarek Auel
+ * @since March 02, 2015.
+ */
+public class HelloWorld {
+
+    Gson gson;
+
+    public static void main(String[] args) {
+        System.out.println("Hello World of IoT");
+    }
+
+}
---
+++ src/test/java/de/dhbw/mannheim/iot/HelloWorldTest.java
@@ -0,0 +1,16 @@
+package de.dhbw.mannheim.iot;
+
+import org.junit.Test;
+import org.junit.Assert;
+
+/**
+ * @author Tarek Auel
+ * @since March 02, 2015.
+ */
+public class HelloWorldTest {
+
+    @Test
+    public void testCase() {
+        Assert.assertEquals(1,1);
+    }
+}
