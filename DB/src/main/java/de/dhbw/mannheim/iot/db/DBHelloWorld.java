0d670ff0a4e210ea083f9a2b2371a9e040e4cc64
michischeid <michischeid@web.de>
2015-03-06 15:15:47 +0100

    added communication between MQ-->RTA and MQ-->DB

--- DB/src/main/java/de/dhbw/mannheim/iot/db/DBHelloWorld.java
+++ DB/src/main/java/de/dhbw/mannheim/iot/db/DBHelloWorld.java
@@ -1,5 +1,10 @@
 package de.dhbw.mannheim.iot.db;
 
+import de.dhbw.mannheim.iot.communication.Message;
+import de.dhbw.mannheim.iot.communication.TcpClient;
+import de.dhbw.mannheim.iot.mq.MQHelloWorld;
+import de.dhbw.mannheim.iot.mq.MQRegistrationMessage;
+
 /**
  * @author Tarek Auel
  * @since March 05, 2015.
@@ -11,7 +16,11 @@ public class DBHelloWorld {
      */
     public static void main(String[] args)
     {
-
+        System.out.println("client started");
+        TcpClient client = new TcpClient(MQHelloWorld.MQ_PORT);
+        client.sendMessage(new MQRegistrationMessage(System.currentTimeMillis(),"ERP"));
+        client.receiveMessage();
+        client.close();
     }
 }
 
--- RTA/src/main/java/de/dhbw/mannheim/iot/rta/RtaDemo.java
+++ RTA/src/main/java/de/dhbw/mannheim/iot/rta/RtaDemo.java
@@ -1,6 +1,8 @@
 package de.dhbw.mannheim.iot.rta;
-
+import de.dhbw.mannheim.iot.communication.TcpClient;
 import de.dhbw.mannheim.iot.model.DemoModel;
+import de.dhbw.mannheim.iot.mq.MQHelloWorld;
+import de.dhbw.mannheim.iot.mq.MQRegistrationMessage;
 
 /**
  * @author Tarek Auel
@@ -8,4 +10,13 @@ import de.dhbw.mannheim.iot.model.DemoModel;
  */
 public class RtaDemo {
     DemoModel model;
+
+    public static void main(String[] args)
+    {
+        System.out.println("client started");
+        TcpClient client = new TcpClient(MQHelloWorld.MQ_PORT);
+        client.sendMessage(new MQRegistrationMessage(System.currentTimeMillis(),"machine"));
+        client.receiveMessage();
+        client.close();
+    }
 }
