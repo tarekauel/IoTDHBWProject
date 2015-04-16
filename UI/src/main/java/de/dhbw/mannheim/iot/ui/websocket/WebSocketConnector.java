d919bd206253126a9ded66d4e59ae61652f81d66
D059166 <marc.becker@sap.com>
2015-04-16 13:26:14 +0200

    updated resources

--- UI/src/main/java/de/dhbw/mannheim/iot/ui/websocket/WebSocketConnector.java
+++ UI/src/main/java/de/dhbw/mannheim/iot/ui/websocket/WebSocketConnector.java
@@ -11,13 +11,19 @@ import org.atmosphere.nettosphere.Nettosphere;
 public class WebSocketConnector {
 
     public static void main(String[] args) {
-        //new WebSocketConnector(RandomLongHandler.class, "/random-long", 4040);
+        new WebSocketConnector(RandomLongHandler.class, "/random-long", 4040);
         new WebSocketConnector(AverageRuntimeHandler.class, "/average-runtime", 5050);
     }
 
     public WebSocketConnector(Class<? extends WebSocketHandler> clazz, String ressourceString, int port) {
         Config.Builder b = new Config.Builder();
-        b.resource(clazz).resource("./webapps").resource("./src/main/resources"+ressourceString).resource("./UI/src/main/resources"+ressourceString).resource("/Users/tarek/IntellijProjects/IoTProjekt/UI/src/main/resources" + ressourceString).port(port).host("localhost").build();
+        b.resource(clazz)
+            .resource("./webapps")
+            .resource("./src/main/resources"+ressourceString)
+            .resource("./UI/src/main/resources"+ressourceString)
+            .resource("./../UI/src/main/resources" + ressourceString)
+            .port(port)
+            .host("localhost").build();
         Nettosphere s = new Nettosphere.Builder().config(b.build()).build();
         s.start();
         log.info("NettoSphere Chat Server started on localhost:" + port + " for class " + clazz.getName());
